a428db3fe818152e7bf3e50fbd292c5056f08400
UENISHI Kota <kuenishi@gmail.com>
2012-05-23 23:51:34 +0900

    wrong impl fixed

--- rt_mweight.py
+++ rt_mweight.py
@@ -35,7 +35,6 @@ def do_rt(authorization, id_str, tw = None):
     if tw is None:  t = twitter.Twitter(auth=authorization)
     else:           t = tw
     r = t.statuses.retweet(id=id_str)
-    print(r)
     return t
 
 def say(authorization, serif, tw = None):
@@ -43,7 +42,6 @@ def say(authorization, serif, tw = None):
     if tw is None:  t = twitter.Twitter(auth=authorization)
     else:           t = tw
     r = t.statuses.update(status=serif)
-    print(r)
     return t
 
 def make_auth():
@@ -63,14 +61,13 @@ if __name__ == '__main__':
 
     for tweet in tweet_iter:
         now = datetime.datetime.now().ctime()
-        if tweet.get('text'): pass
-        else:                 continue
+        if not tweet.get('user'): continue
 
         u = tweet['user']
         if u['screen_name'] == 'mikansp':
-            print('\n@mikansp said: %s %s' % (tweet['text'], now))
+            print(' @mikansp said: %s %s' % (tweet['text'], now))
             if tweet['text'][:10] == 'My weight:':
-                t = do_rt(authorization, tweet['id_str'], tw=None)
+                t = do_rt(authorization, tweet['id_str'])
                 weight = float(tweet['text'][11:15])
                 serif = "[mikansp forecast] %.1f kg. (%s)" % ((weight + 0.7), now)
                 say(authorization, serif, tw=t)
